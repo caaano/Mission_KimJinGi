3c46897c78c8324e1abc49f9bececadcca04523d
caaano <wlsrl0515@naver.com>
2023-11-07 17:19:49 +0900

    6단계

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -5,51 +5,90 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
+    Scanner scanner;
+    int lastQuotaionId; // 명언의 ID 값을 관리하기 위한 변수로, 초기값은 0으로 설정
+    List<Quotation> quotations; // 명언 객체(Quotation)를 저장하기 위한 리스트를 생성
+
+    App() {
+        scanner = new Scanner(System.in);
+        lastQuotaionId = 0;
+        quotations = new ArrayList<>();
+    }
+
     void run() {
         System.out.println("== 명언 앱 ==");
-
-        int lastQuotaionId = 0;
-
-        List<Quotation> quotations = new ArrayList<>();
 
         while (true) {
             System.out.print("명령) ");
 
-            Scanner scanner = new Scanner(System.in);
-            String cmd = scanner.nextLine();
+            String cmd = scanner.nextLine(); // 사용자가 입력한 명령을 읽어와 cmd 변수에 저장
 
 
             if (cmd.equals("종료")) {
                 break;
             } else if (cmd.equals("등록")) {
-                System.out.print("명언 : ");
-                String content = scanner.nextLine();
-
-                System.out.print("작가 : ");
-                String authorName = scanner.nextLine();
-
-                lastQuotaionId++;
-                int id = lastQuotaionId;
-
-                Quotation quotation = new Quotation(id, content, authorName);
-                quotations.add(quotation);
-
-                System.out.printf("%d번 명언이 등록 되었습니다.\n", lastQuotaionId);
+                actionWrite();
             } else if (cmd.equals("목록")) {
-
-                System.out.println("번호 / 작가 / 명언");
-
-                System.out.println("-------------------------------------------");
-
-                if (quotations.isEmpty()){
-                    System.out.println("등록된 명언이 없습니다.");
-                }
-                for (int i = quotations.size()-1; i >= 0; i--) {
-                    Quotation quotation = quotations.get(i);
-                    System.out.printf("%d / %s / %s\n", quotation.id, quotation.authorName, quotation.content);
-                }
+                actionList();
+            } else if (cmd.startsWith("삭제?")) {
+                actionRemove(cmd);
             }
-
         }
+    }
+
+    // 리팩토링
+    void actionWrite() {
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
+
+        System.out.print("작가 : ");
+        String authorName = scanner.nextLine();
+
+        lastQuotaionId++;
+        int id = lastQuotaionId;
+
+        Quotation quotation = new Quotation(id, content, authorName);
+        quotations.add(quotation);
+
+        System.out.printf("%d번 명언이 등록 되었습니다.\n", lastQuotaionId);
+    }
+
+    void actionList() {
+        System.out.println("번호 / 작가 / 명언");
+
+        System.out.println("-------------------------------------------");
+
+        if (quotations.isEmpty()) {
+            System.out.println("등록된 명언이 없습니다.");
+        }
+        for (int i = quotations.size() - 1; i >= 0; i--) {
+            Quotation quotation = quotations.get(i);
+            System.out.printf("%d / %s / %s\n", quotation.id, quotation.authorName, quotation.content);
+        }
+    }
+
+    void actionRemove(String cmd){
+        String[] cmdBits = cmd.split("\\?", 2);
+        String action = cmdBits[0];
+        String queryString = cmdBits[1];
+
+        String[] queryStringBits = queryString.split("&");
+
+        int id = 0;
+
+        for (int i = 0; i < queryStringBits.length; i++) {
+            String queryParamStr = queryStringBits[i];
+
+            String[] queryParamStrBits = queryParamStr.split("=", 2);
+
+            String paramName = queryParamStrBits[0];
+            String paramValue = queryParamStrBits[1];
+
+            if (paramName.equals("id")) {
+                id = Integer.parseInt(paramValue);
+            }
+        }
+
+        System.out.printf("%d번 명언이 삭제 되었습니다.\n", id);
     }
 }
